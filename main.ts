15107f5c975cf8a634b98875841666e489af5efa
Fran Diaz <100407685+ProfeFDV@users.noreply.github.com>
2022-02-28 16:26:42 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P2, function () {
+    music.playTone(139, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+})
+basic.forever(function () {
+	
+})
